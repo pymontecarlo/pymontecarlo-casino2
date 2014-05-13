48550163aec5bae00f03f45dc7578494714840b4
Philippe Pinard <philippe.pinard@gmail.com>
2014-05-13 13:09:27 +0200

    Make worker create the .sim files.

--- pymontecarlo/program/casino2/worker.py
+++ pymontecarlo/program/casino2/worker.py
@@ -19,13 +19,13 @@ __copyright__ = "Copyright (c) 2012 Philippe T. Pinard"
 __license__ = "GPL v3"
 
 # Standard library modules.
-import os
-import logging
+#import os
+#import logging
 
 # Third party modules.
 
 # Local modules.
-from pymontecarlo.settings import get_settings
+#from pymontecarlo.settings import get_settings
 from pymontecarlo.program.worker import Worker as _Worker
 
 # Globals and constants variables.
@@ -37,11 +37,12 @@ class Worker(_Worker):
         """
         _Worker.__init__(self, program)
 
-        self._executable = get_settings().casino2.exe
-        if not os.path.isfile(self._executable):
-            raise IOError('Casino 2 executable (%s) cannot be found' % self._executable)
-        logging.debug('Casino 2 executable: %s', self._executable)
+#        self._executable = get_settings().casino2.exe
+#        if not os.path.isfile(self._executable):
+#            raise IOError('Casino 2 executable (%s) cannot be found' % self._executable)
+#        logging.debug('Casino 2 executable: %s', self._executable)
 
     def run(self, options, outputdir, workdir, *args, **kwargs):
-        raise NotImplementedError("Simulations with Casino2 cannot be directly run. " + \
-            "Please use the create method to create the .sim files and run them in Casino 2.")
+        self.create(options, outputdir, *args, **kwargs)
+        raise NotImplementedError("Simulations with Casino2 cannot be directly run. "
+            "The .sim file was created in the output directory.")
