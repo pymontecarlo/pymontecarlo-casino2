353e646e716d366c9339cdf0015cbf7f424d4ce5
Philippe Pinard <philippe.pinard@gmail.com>
2017-03-19 19:12:38 +0000

    Update to new handler format

--- pymontecarlo_casino2/fileformat/program.py
+++ pymontecarlo_casino2/fileformat/program.py
@@ -14,14 +14,26 @@ from pymontecarlo_casino2.program import Casino2Program
 
 class Casino2ProgramHDF5Handler(HDF5Handler):
 
-    CLASS = Casino2Program
-
     ATTR_EXECUTABLE = 'executable'
 
+    def _parse_executable(self, group):
+        return group.attrs.get(self.ATTR_EXECUTABLE).decode('ascii')
+
+    def can_parse(self, group):
+        return super().can_parse(group) and \
+            self.ATTR_EXECUTABLE in group.attrs
+
     def parse(self, group):
-        executable = group.attrs.get(self.ATTR_EXECUTABLE).decode('ascii')
+        executable = self._parse_executable(group)
         return Casino2Program(executable)
 
-    def convert(self, obj, group):
-        super().convert(obj, group)
-        group.attrs[self.ATTR_EXECUTABLE] = np.string_(obj.executable)
+    def _convert_executable(self, executable, group):
+        group.attrs[self.ATTR_EXECUTABLE] = np.string_(executable)
+
+    def convert(self, program, group):
+        super().convert(program, group)
+        self._convert_executable(program.executable, group)
+
+    @property
+    def CLASS(self):
+        return Casino2Program
