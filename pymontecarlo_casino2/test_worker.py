f148c88a4ad9916e19ce6ac4951fc8b6d992ced4
Philippe Pinard <philippe.pinard@gmail.com>
2017-07-15 18:07:55 +0100

    Fix bug

--- pymontecarlo_casino2/test_worker.py
+++ pymontecarlo_casino2/test_worker.py
@@ -19,7 +19,7 @@ from pymontecarlo_casino2.worker import Casino2Worker
 
 # Globals and constants variables.
 
-@unittest.skipUnless(pymontecarlo.settings.has_program('casino2'),
+@unittest.skipUnless(pymontecarlo.settings.is_program_activated('casino2'),
                      'Casino 2 should be configured to run these tests')
 class TestCasino2Worker(TestCase):
 
