8f76ebf11c61f72420ed42c1732fc17e0381a817
Philippe Pinard <philippe.pinard@gmail.com>
2021-12-29 13:28:34 +0000

    Update CI, readme

--- pymontecarlo_casino2/conftest.py
+++ pymontecarlo_casino2/conftest.py
@@ -21,12 +21,13 @@ from pymontecarlo_casino2.program import Casino2Program
 
 # Globals and constants variables.
 
-@pytest.yield_fixture(scope='session')
+
+@pytest.fixture(scope="session")
 def event_loop(request):
     """
     Run all tests using the default event loop and never closes it.
     """
-    if sys.platform == 'win32':
+    if sys.platform == "win32":
         asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy())
 
     loop = asyncio.new_event_loop()
@@ -34,16 +35,18 @@ def event_loop(request):
     yield loop
     loop.close()
 
+
 @pytest.fixture
 def options():
     program = Casino2Program(number_trajectories=50)
     beam = GaussianBeam(15e3, 10e-9)
     sample = SubstrateSample(Material.pure(29))
-    detector = PhotonDetector('xray', math.radians(40.0))
+    detector = PhotonDetector("xray", math.radians(40.0))
     analyses = [PhotonIntensityAnalysis(detector)]
-    tags = ['basic', 'test']
+    tags = ["basic", "test"]
     return Options(program, beam, sample, analyses, tags)
+
 
 @pytest.fixture
 def testdatadir():
-    return os.path.abspath(os.path.join(os.path.dirname(__file__), 'testdata'))
+    return os.path.abspath(os.path.join(os.path.dirname(__file__), "testdata"))
